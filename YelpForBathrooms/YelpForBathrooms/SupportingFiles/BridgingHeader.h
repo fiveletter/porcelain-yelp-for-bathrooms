eb09724b4a7e55501c6879e48d1fd25d020e40e7
Mitchell Waldman <mitchellwaldman@Mitchells-MacBook-Pro.local>
2015-11-21 18:12:34 -0800

    Put in bridging for Google API

---
+++ YelpForBathrooms/YelpForBathrooms/SupportingFiles/BridgingHeader.h
@@ -0,0 +1,9 @@
+//
+//  BridgingHeader.h
+//  YelpForBathrooms
+//
+//  Created by Mitchell Waldman on 11/21/15.
+//  Copyright © 2015 Five Letter. All rights reserved.
+//
+
+#import "../../Pods/GoogleSignIn/Headers/Public/GoogleSignIn.h"
