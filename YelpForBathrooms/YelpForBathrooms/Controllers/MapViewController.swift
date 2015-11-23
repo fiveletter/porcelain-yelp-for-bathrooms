65e3c343549e8b8065ab2eebffd899d8c0a2278f
Mitchell Waldman <mitchellwaldman@Mitchells-MacBook-Pro.local>
2015-11-22 17:25:53 -0800

    Add in google map to main view.

--- YelpForBathrooms/YelpForBathrooms/Controllers/MapViewController.swift
+++ YelpForBathrooms/YelpForBathrooms/Controllers/MapViewController.swift
@@ -7,15 +7,21 @@
 //
 
 import UIKit
+import GoogleMaps
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
     
     var logged_in = true
     var model: Int = 1219
     
+    let locationManager = CLLocationManager()
+    
+    @IBOutlet weak var mapView: GMSMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
         // Do any additional setup after loading the view.
     }
 
@@ -23,6 +29,7 @@ class MapViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     @IBAction func openProfileScreen() {
         
         if UserManager.sharedInstance.IsSignedIn {
@@ -33,11 +40,10 @@ class MapViewController: UIViewController {
             self.performSegueWithIdentifier("loginSegue", sender: self)
         }
     }
+    
     @IBAction func addBathroom() {
         
     }
-    
-
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -54,6 +60,22 @@ class MapViewController: UIViewController {
             }
         }
     }
+    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status == .AuthorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+            
+            mapView.myLocationEnabled = true
+            mapView.settings.myLocationButton = true
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
 
+            locationManager.stopUpdatingLocation()
+        }
+        
+    }
 
 }
